7f566cc41a9a630b99d8313f03520bbaa39ededa
onunu <or2onunu@gmail.com>
2023-01-23 22:10:29 +0200

    change

--- src/app/sec-to-min.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { SecToMinPipe } from './sec-to-min.pipe';
-
-describe('SecToMinPipe', () => {
-  it('create an instance', () => {
-    const pipe = new SecToMinPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
