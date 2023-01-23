705872d5fd7bf2bf12074df2dc8f0353e4764359
onunu <or2onunu@gmail.com>
2023-01-23 22:16:27 +0200

    change

---
+++ back/players-array.js
@@ -0,0 +1,34 @@
+const playersDefault = [
+    {
+        color: 'white',
+        points: 0
+    },
+    {
+        color: 'red',
+        points: 0
+    },
+    {
+        color: 'brown',
+        points: 0
+    },
+    {
+        color: 'purple',
+        points: 0
+    },
+    {
+        color: 'green',
+        points: 0
+    },
+    {
+        color: 'marble',
+        points: 0
+    },
+    {
+        color: 'orange',
+        points: 0
+    },
+    {
+        color: 'blue',
+        points: 0
+    },
+]
