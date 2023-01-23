2e27340707ff74ba5f7f6f577359d53aa6e7059e
onunu <or2onunu@gmail.com>
2023-01-23 20:47:37 +0200

    client

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {PlayersService} from "./players.service";
+import {Observable} from "rxjs";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,40 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'project';
+  playersObs: Observable<any[]>;
+  timerTime: number;
+  private playersConst: any[];
+
+   isRollDiceTime = false;
+
+  constructor(private playersService:PlayersService) {
+    this.playersObs = playersService.playersConst;
+  }
+
+  ngOnInit(){
+    this.playersService.playersConst.subscribe(val => {
+      this.playersConst = val;
+      this.timerTime = 5;
+      this.isRollDiceTime = true;
+    })
+  }
+
+  changeTurns() {
+    if(!this.isRollDiceTime) {
+      this.playersService.changeTurns();
+    } else {
+      this.isRollDiceTime = false;
+      if (this.playersConst[0].points < 6)
+        this.timerTime = 8;
+      else if (this.playersConst[0].points < 10)
+        this.timerTime = 12
+      else
+        this.timerTime = 18
+    }
+  }
+
+  newGame() {
+    this.isRollDiceTime = false;
+    this.playersService.newPlayersConst();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PlayerComponent } from './player/player.component';
+import { TimerComponent } from './timer/timer.component';
+import { SecToMinPipe } from './sec-to-min.pipe';
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PlayerComponent,
+    TimerComponent,
+    SecToMinPipe
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        FormsModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
